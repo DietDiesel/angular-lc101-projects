4c7bfc84bbd626003bce8df410b4809838ee0cf3
swankidelic <swankidelic@gmail.com>
2021-08-11 20:49:34 -0500

    another commit lol

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 's S s t T t y Y y l L l e E e';
   image1 = 'https://ircimg.net/DQ947h3.jpg';
   image2 = 'https://ircimg.net/1367670634512.jpg';
   image3 = 'https://ircimg.net/941470_452438994831159_1965679624_n.jpg';
