b855753500fb1413b5bb9abf40c8c9c68652a660
swankidelic <swankidelic@gmail.com>
2021-08-15 20:15:11 -0500

    completed exercise

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   goldActive: boolean = true;
+   silverActive: boolean = true;
+   copperActive: boolean = true;
+   slipperyButton: number = 100;
 
    constructor() { }
 
    ngOnInit() { }
 
+   deactivateButton(button: string = null){
+      switch (button) {
+         case "gold":
+            this.goldActive = false;
+            break;
+         case "silver":
+            this.silverActive = false;
+            break;
+         case "copper":
+            this.copperActive = false;
+            break;
+      }
+   }
+
+   toggleButtons() {
+      this.goldActive = true;
+      this.silverActive = true;
+      this.copperActive = true;
+   }
 }
